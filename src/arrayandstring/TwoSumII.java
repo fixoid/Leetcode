deb612ce6bce0c7689c21b47aa7453a70f0558d6
fixoid <fixoid@bk.ru>
2022-03-28 23:42:28 +0200

    167. Two Sum II - Input array is sorted (py + java)

---
+++ src/arrayandstring/TwoSumII.java
@@ -0,0 +1,61 @@
+package arrayandstring;
+
+// 167. Two Sum II - Input array is sorted
+
+// Solution
+// Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, 
+// find two numbers such that they add up to a specific target number. 
+// Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+// Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+// The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+// Your solution must use only constant extra space.
+// Example 1: Input: numbers = [2,7,11,15], target = 9 Output: [1,2]
+// Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+
+// Example 2: Input: numbers = [2,3,4], target = 6 Output: [1,3]
+// Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+
+// Example 3: Input: numbers = [-1,0], target = -1 Output: [1,2]
+// Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
+ 
+// Constraints:
+// 2 <= numbers.length <= 3 * 104
+// -1000 <= numbers[i] <= 1000
+// numbers is sorted in non-decreasing order.
+// -1000 <= target <= 1000
+// The tests are generated such that there is exactly one solution.
+
+public class TwoSumII {
+    public static int[] twoSum(int[] numbers, int target) {
+        
+        int low = 0;
+        int high = numbers.length - 1;
+        while (low < high) {
+            int sum = numbers[low] + numbers[high];
+            if (sum == target) {
+                int ans[] = new int[] {low+1, high+1};
+                return ans;
+            }
+            else if (sum < target) {
+                low ++;
+            }
+            else {
+                high --;
+            }
+        }
+        return new int[0];
+    }
+
+    private static void printArray(int[] a) {
+        for (int i = 0; i < a.length; ++i) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int numbers[] = new int[] {12,13,23,28,43,44,59,60,61,68,70,86,88,92,124,125,136,168,173,173,180,199,212,221,227,230,277,282,306,314,316,321,325,328,336,337,363,365,368,370,370,371,375,384,387,394,400,404,414,422,422,427,430,435,457,493,506,527,531,538,541,546,568,583,585,587,650,652,677,691,730,737,740,751,755,764,778,783,785,789,794,803,809,815,847,858,863,863,874,887,896,916,920,926,927,930,933,957,981,997};
+        int target = 542;
+        printArray( twoSum(numbers, target) );
+    }
+}
